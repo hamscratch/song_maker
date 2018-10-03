e29b20c8b0c2d5c826490792f15dd72e9e080cd3
hamscratch <hamscratch@users.noreply.github.com>
2018-10-03 16:43:49 -0400

    Update song_util.php

--- song_util.php
+++ song_util.php
@@ -13,9 +13,10 @@ $song_keys = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'];
 $song_tempos = ['85', '90', '95', '100', '105', '110', '115', '120', '125', '130'];
 $chord_numbers = ['I', 'ii', 'iii', 'IV', 'V', 'vi'];
 $time_signatures = ['4/4', '3/4', '6/8'];
+$chords = '';
 
 // Chords in the Key:
-$key_of_c_major = ['C', 'Dm', 'Em', 'F', 'G', 'Am'];
+$key_of_c_major = ["I"=>"C","ii"=>"Dm","iii"=>"Em","IV"=>"F","V"=>"G","vi"=>"Am"];
 $key_of_db_major = ['C', 'Dm', 'Em', 'F', 'G', 'Am'];
 $key_of_d_major = ['C', 'Dm', 'Em', 'F', 'G', 'Am'];
 $key_of_eb_major = ['C', 'Dm', 'Em', 'F', 'G', 'Am'];
@@ -29,13 +30,9 @@ $key_of_bb_major = ['C', 'Dm', 'Em', 'F', 'G', 'Am'];
 $key_of_b_major = ['C', 'Dm', 'Em', 'F', 'G', 'Am'];
 
 function pickAKey($song_keys) {
-	$song_key = array_rand(array_flip($song_keys));
-
-	if ($song_key == 'C') {
-		$chords = $key_of_c_major;
-	}
-
-	echo "Song Key: $song_key \nChords: $chords\n";
+	$song_key = 'C'; //array_rand(array_flip($song_keys));
+	
+	echo "Song Key: $song_key \n";
 }
 
 function pickATempo($song_tempos) {
@@ -44,6 +41,7 @@ function pickATempo($song_tempos) {
 	echo "Tempo: $song_tempo \n";
 }
 
+// probably need to do a for each loop here instead. that makes sense. thanks sara!
 function pickChordNumbersVerse($chord_numbers) {
 	$chord_number_1 = array_rand(array_flip($chord_numbers));
 	$chord_number_2 = array_rand(array_flip($chord_numbers));
@@ -68,10 +66,16 @@ function pickASignature($time_signatures) {
 	echo "Time Signature: $time_signature \n";
 }
 
-
+function showTheChords($song_key) {
+	$key_of_c_major = ["I"=>"C" ,"ii"=>"Dm" ,"iii"=>"Em" ,"IV"=>"F" ,"V"=>"G" ,"vi"=>"Am"];
+	$chords = $key_of_c_major;
+	
+	echo 'Chords: ' . implode(", ", $chords) . "\n";
+}
 
 pickAKey($song_keys);
 pickATempo($song_tempos);
 pickASignature($time_signatures);
+showTheChords($chords);
 pickChordNumbersVerse($chord_numbers);
 pickChordNumbersChorus($chord_numbers);
